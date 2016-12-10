2c83d144b4c4fe98bffab82271606a70c45c8b6f
Cameron Yee <makakoa57@gmail.com>
2016-12-09 18:32:18 -0800

    colorize waveform, white text

--- scripts/levels.js
+++ scripts/levels.js
@@ -12,27 +12,42 @@ AFRAME.registerComponent('levels', {
   init: function() {
     this.analyser = this.data.analyserEl.components.audioanalyser;
 
-    for (var i = 0; i < this.analyser.levels.length; i++) {
-      var point = document.createElement('a-box');
+    for (var i = 0; i < this.analyser.levels.length; i += 2) {
+      var point = document.createElement('a-entity');
       point.setAttribute('position', {
-        x: (i / 10) - (this.analyser.levels.length / 20),
+        x: (i / 20) - (this.analyser.levels.length / 40),
         y: 0,
         z: -10
       });
-      point.setAttribute('color', '#dddddd');
-      point.setAttribute('width', '0.1');
-      point.setAttribute('height', '0.1');
-      point.setAttribute('depth', '0.1');
+      point.setAttribute('geometry', {
+        primitive: 'box',
+        width: 0.1,
+        height: 0.1,
+        depth: 0.1
+      });
+      point.setAttribute('material', {
+        color: '#dddddd'
+      });
       this.el.appendChild(point);
     }
   },
 
   tick: function() {
     var children = this.el.children;
-    for (var i = 0; i < this.analyser.levels.length; i++) {
-      if (children[i]) {
-        children[i].setAttribute('position', {
-          x: (i / 10) - (this.analyser.levels.length / 20),
+    var total = this.analyser.levels.length;
+    var qrt = total / 4;
+    var hf = total / 2;
+    for (var i = 0; i < this.analyser.levels.length; i += 2) {
+      if (children[i / 2]) {
+        children[i/2].setAttribute('material', {
+          color: 'rgb(' + [
+            Math.floor(i < hf ? 255 - Math.abs(qrt - i) / qrt * 255 : 0),
+            Math.floor(i > qrt && i < 3 * qrt ? 255 - Math.abs(hf - i) / qrt * 255 : 0),
+            Math.floor(i > hf ? 255 - Math.abs(qrt*3 - i) / qrt * 255 : 0)
+          ].join(',') + ')'
+        });
+        children[i / 2].setAttribute('position', {
+          x: (i / 20) - (this.analyser.levels.length / 40),
           y: this.analyser.levels[i] / 50,
           z: -10
         });
