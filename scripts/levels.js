8fffc3283b9da9cd4f92b6c2301d98bef681278c
Cameron Yee <makakoa57@gmail.com>
2016-12-10 10:16:08 -0800

    volume graph

--- scripts/levels.js
+++ scripts/levels.js
@@ -2,6 +2,8 @@
 
 /* global AFRAME */
 
+var downScale = 3;
+
 AFRAME.registerComponent('levels', {
   dependencies: ['analyser'],
 
@@ -12,10 +14,10 @@ AFRAME.registerComponent('levels', {
   init: function() {
     this.analyser = this.data.analyserEl.components.audioanalyser;
 
-    for (var i = 0; i < this.analyser.levels.length; i += 2) {
+    for (var i = 0; i < this.analyser.levels.length; i += downScale) {
       var point = document.createElement('a-entity');
       point.setAttribute('position', {
-        x: (i / 20) - (this.analyser.levels.length / 40),
+        x: i / (10 * downScale),
         y: 0,
         z: -10
       });
@@ -25,29 +27,28 @@ AFRAME.registerComponent('levels', {
         height: 0.1,
         depth: 0.1
       });
+
+      var total = this.analyser.levels.length;
+      var qrt = total / 4;
+      var hf = total / 2;
       point.setAttribute('material', {
-        color: '#dddddd'
+        color: 'rgb(' + [
+          Math.floor(i < hf ? 255 - Math.abs(qrt - i) / qrt * 255 : 0),
+          Math.floor(i > qrt && i < 3 * qrt ? 255 - Math.abs(hf - i) / qrt * 255 : 0),
+          Math.floor(i > hf ? 255 - Math.abs(qrt*3 - i) / qrt * 255 : 0)
+        ].join(',') + ')'
       });
+
       this.el.appendChild(point);
     }
   },
 
   tick: function() {
     var children = this.el.children;
-    var total = this.analyser.levels.length;
-    var qrt = total / 4;
-    var hf = total / 2;
-    for (var i = 0; i < this.analyser.levels.length; i += 2) {
-      if (children[i / 2]) {
-        children[i/2].setAttribute('material', {
-          color: 'rgb(' + [
-            Math.floor(i < hf ? 255 - Math.abs(qrt - i) / qrt * 255 : 0),
-            Math.floor(i > qrt && i < 3 * qrt ? 255 - Math.abs(hf - i) / qrt * 255 : 0),
-            Math.floor(i > hf ? 255 - Math.abs(qrt*3 - i) / qrt * 255 : 0)
-          ].join(',') + ')'
-        });
-        children[i / 2].setAttribute('position', {
-          x: (i / 20) - (this.analyser.levels.length / 40),
+    for (var i = 0; i < this.analyser.levels.length; i += downScale) {
+      if (children[i / downScale]) {
+        children[i / downScale].setAttribute('position', {
+          x: i / (10 * downScale),
           y: this.analyser.levels[i] / 50,
           z: -10
         });
---
+++ scripts/volume-graph.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/* global AFRAME */
+
+var maxChildren = 100,
+    ticksPerPoint = 3;
+
+AFRAME.registerComponent('volume-graph', {
+  dependencies: ['analyser'],
+
+  schema: {
+    analyserEl: {type: 'selector'}
+  },
+
+  init: function() {
+    this.analyser = this.data.analyserEl.components.audioanalyser;
+    this.count = 0;
+  },
+
+  tick: function() {
+
+    // Shift
+    for (var c = 0; c < this.el.children.length; c++) {
+      var child = this.el.children[c];
+      var position = child.getComputedAttribute('position');
+      child.setAttribute('position', {
+        x: position.x + 0.1,
+        y: position.y,
+        z: position.z
+      });
+    }
+
+    // Add new point every ticksPerPoint ticks
+    this.count++;
+    if (this.count % ticksPerPoint > 0) return;
+
+    // Max 20 points
+    if (this.el.children.length > maxChildren) this.el.removeChild(this.el.children[0]);
+
+    var totalVol = 0;
+    for (var i = 0; i < this.analyser.levels.length; i += 50) {
+      totalVol += this.analyser.levels[i];
+    }
+    var point = document.createElement('a-entity');
+    point.setAttribute('geometry', {
+      primitive: 'box',
+      width: 0.2,
+      height: totalVol / 300, // rough est of max / 10
+      depth: 0.2
+    });
+    point.setAttribute('material', {
+      color: 'white'
+    });
+    point.setAttribute('position', {
+      x: 0,
+      y: totalVol / 600,
+      z: 0
+    });
+    this.el.appendChild(point);
+
+  }
+});
